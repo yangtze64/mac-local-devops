ddf195e7360685cd9e6ebbbcec3db1d6ffb7ecd5
ycj <594998624@qq.com>
2023-03-12 21:45:13 +0800

    jaeger

--- jaeger/start.sh
+++ jaeger/start.sh
@@ -5,4 +5,7 @@ docker run -d \
 --name jaeger-all-in-one \
 --restart always \
 -p 5775:5775/udp -p 16686:16686 -p 14250:14250 -p 14268:14268 \
+-v /usr/share/zoneinfo/Asia/Shanghai:/etc/localtime \
+-v $DEVOPS_HOME/etc/timezone:/etc/timezone \
+-v $JAEGER_PATH/data:/tmp \
 jaegertracing/all-in-one:latest
